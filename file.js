94afe04ccf5fe5d9cf12ff6924111c87eeb20455
timidij <timidiodubo@gmail.com>
2022-06-24 05:30:31 +0700

    almost there

--- file.js
+++ file.js
@@ -1,23 +1,23 @@
 // alert("hello world")
 var number1 = parseFloat(prompt("enter first number"));
-var number2 = parseFloat(prompt("enter first number"));
+var number2 = parseFloat(prompt("enter second number"));
 var opp = prompt("enter operator +, / , * , - ");
 var result;
 
 
 if (opp == "+"){
 result = number1 + number2;
- alert( number1 +"+"+ number2 +"=" +result)
+ alert( number1 +" + "+ number2 +" = " +result)
 }
 
 else if  (opp == "-"){
     result = number1 - number2;
     
-    alert( number1 +"-"+ number2 +"=" +result)
+    alert( number1 +" - "+ number2 +" = " +result)
 }
 else if (opp == "*"){
     result = number1 * number2;
-    alert( number1 +"*"+ number2 +"=" +result)
+    alert( number1 +" * "+ number2 +"=" +result)
 }
 else if (opp == "/"){
     result = number1 / number2;
